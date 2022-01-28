6b7fb54b91e6ab0c86fe3156bea6a7a107856fe6
JuanROliva <juan.oliva90@gmail.com>
2022-01-28 14:17:29 -0600

    Avance No. 1

--- src/lab2p2_juanoliva_10741313/Animal.java
+++ src/lab2p2_juanoliva_10741313/Animal.java
@@ -1,13 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab2p2_juanoliva_10741313;
-
-/**
- *
- * @author Juan Oliva
- */
 public class Animal {
     
+    private String nombreCientifico;
+    private String nombreComun;
+    private String habitat;
+    private String alimentacion;
+    private String descripcionRasgos;
+    private String distribucionGeografica;
+    private int vida;
+
+    public Animal() {
+    }
+
+    public Animal(String nombreCientifico, String nombreComun, String habitat, String alimentacion, String descripcionRasgos, String distribucionGeografica, int vida) {
+        this.nombreCientifico = nombreCientifico;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.descripcionRasgos = descripcionRasgos;
+        this.distribucionGeografica = distribucionGeografica;
+        this.vida = vida;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getDescripcionRasgos() {
+        return descripcionRasgos;
+    }
+
+    public void setDescripcionRasgos(String descripcionRasgos) {
+        this.descripcionRasgos = descripcionRasgos;
+    }
+
+    public String getDistribucionGeografica() {
+        return distribucionGeografica;
+    }
+
+    public void setDistribucionGeografica(String distribucionGeografica) {
+        this.distribucionGeografica = distribucionGeografica;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public boolean setVida(int vida) {
+        if (vida>0) {
+            this.vida = vida;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void comer(Animal a){
+        vida += a.getVida();
+    }
 }
--- src/lab2p2_juanoliva_10741313/Lab2P2_JuanOliva_10741313.java
+++ src/lab2p2_juanoliva_10741313/Lab2P2_JuanOliva_10741313.java
@@ -1,6 +1,76 @@
 package lab2p2_juanoliva_10741313;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2P2_JuanOliva_10741313 {
+    
+    static ArrayList<Animal> listaAnimales = new ArrayList<Animal>();
+    
     public static void main(String[] args) {
+        encabezado();
+        boolean ciclo = true;
+        while (ciclo) {
+            String opc = menu();
+            
+            
+            
+        }
+        
+    }
+    
+    
+    static void encabezado(){
+        System.out.println("-----------------------------------------------------");
+        System.out.println(" Universidad Tecnologica Centroamericana UNITEC");
+        System.out.println(" Asignatura: Laboratorio Programacion II");
+        System.out.println(" Alumno: Juan Rafael Oliva");
+        System.out.println(" Cuenta: 10741313");
+    }
+    
+    static String menu(){
+        System.out.println("-----------------------------------------------------");
+        System.out.println("""
+                           1. Agregar un Animal
+                           2. Modificar Animal
+                           3. Eliminar Animal
+                           4. Imprimir Animal en posicion especifica
+                           5. Imprimir todos los animales
+                           6. Imprimir por Nombre Cientifico
+                           7. 
+                           """);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Ingrese la opcion que desea ejecutar");
+        Scanner lea = new Scanner(System.in);
+        return lea.next();
+    }
+    
+    
+    static boolean eliminarAnimal(String nombreCientifico){
+        nombreCientifico.toLowerCase();
+        int pos = retornoPosicionAnimal(nombreCientifico);
+        if (pos!=-1) {
+            listaAnimales.remove(pos);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+    static boolean hayAnimales(){
+        return listaAnimales.size()!=-1;
+    }
+    
+    
+    static int retornoPosicionAnimal(String nombreCientifico){
+        nombreCientifico.toLowerCase();
+        for (Animal a : listaAnimales) {
+            if (a.getNombreCientifico().toLowerCase().equals(nombreCientifico)) {
+                return listaAnimales.indexOf(a);
+            }
+        }
+        return -1;
     }
     
 }
